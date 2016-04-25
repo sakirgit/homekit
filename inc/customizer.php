60385f91325c21abc7525246fd2a4f23dccfb0ee
sakirgit <sakiremail@gmail.com>
2016-04-26 03:23:29 +0600

    Start HomeKit

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * HomeKit Theme Customizer.
+ *
+ * @package HomeKit
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function homekit_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'homekit_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function homekit_customize_preview_js() {
+	wp_enqueue_script( 'homekit_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'homekit_customize_preview_js' );
